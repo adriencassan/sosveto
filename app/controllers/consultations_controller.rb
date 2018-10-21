166b22f110810c3d7a4c2cca1d5038107ac8398e
Adrien Cassan <adrien.cassan.tech@gmail.com>
2018-10-21 14:52:39 +0200

    validates first test for pdf

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -54,14 +54,38 @@ class ConsultationsController < ApplicationController
 
     #  render pdf: "test_name", template: "consultations/pdf/show.html.erb", layout: 'pdf.html.erb', page_size: "A4"
 
-    pdf = render_to_string pdf: "some_file_name", template: "consultations/pdf/show.html.erb", layout: 'pdf.html.erb', page_size: "A4", encoding: "UTF-8"
-    @consultation.report = pdf
-    @consultation.save!
+    pdf_file = render_to_string pdf: "some_file_name", template: "consultations/pdf/show.html.erb", layout: 'pdf.html.erb', page_size: "A4", encoding: "UTF-8"
+
+    #File.open(pdf) do |f|
+    #  @consultation.report = f
+    #end
+
+
+
+    #@consultation.report  = WickedPdf.new.pdf_from_string(render_to_string("consultations/pdf/show.html.erb", layout: 'pdf.html.erb', page_size: "A4", encoding: "UTF-8", layout: 'pdf'))
+    #@consultation.report = pdf
+    #@consultation.save!
     # then save to a file
     #save_path = Rails.root.join('pdfs','filename.pdf')
     #File.open(save_path, 'wb') do |file|
     #  file << pdf
     #end
+
+
+    # Write it to tempfile
+    tempfile = Tempfile.new(['invoice', '.pdf'], Rails.root.join('tmp'))
+    tempfile.binmode
+    tempfile.write pdf_file
+    tempfile.close
+
+    # Attach that tempfile to the invoice
+    unless pdf_file.blank?
+        #@consultation.clear
+        @consultation.update(report: File.open(tempfile.path))
+        tempfile.unlink
+    end
+
+    redirect_to "root"
   end
 
 
